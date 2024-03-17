1763effb6f8fdda28fcef8b9e041cc416bf9c212
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2024-03-18 00:31:28 +1000

    🗣️ Add Discord link

--- honafx/public/main.js
+++ honafx/public/main.js
@@ -1,10 +1,15 @@
 export default {
-    defaultTheme: 'dark', // Force to Dark theme
-    iconLinks: [
-        {
-          icon: 'github',
-          href: 'https://github.com/Hona/VerticalSliceArchitecture.Documentation',
-          title: 'GitHub'
-        }
-    ]
+  defaultTheme: 'dark', // Force to Dark theme
+  iconLinks: [
+    {
+      icon: 'discord',
+      href: 'https://discord.gg/Amwk73e23Z',
+      title: 'Discord'
+    },
+    {
+      icon: 'github',
+      href: 'https://github.com/Hona/VerticalSliceArchitecture.Documentation',
+      title: 'GitHub'
+    }
+  ]
 }
